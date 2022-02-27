b08f07e41fe0e2c6fb8589bcb4e8c24ea1a38c54
Roselynn <worklaptop@Roselynns-MacBook-Pro.local>
2022-02-27 18:49:28 -0500

    new updates

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
+//import files & Packages up here
 const express = require('express');
+const morgan = require('morgan');
+const topSpots = require('./data');
 
-
+//Create your express server below
 const app = express();
+//add your routes & middleware below
+
+app.use(morgan('dev'));
+
+//Get/respond
+app.get('/', function(req, res){
+  console.log('I want to get a repsonse')
+  res.send("200")
+});
+//Get/ Data responds with all top spots
+app.get('/data', function(req,res){
+  console.log("Please send me the topspot data")
+  res.send(topSpots);
+})
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
-
+//export the express application
 module.exports = app;
